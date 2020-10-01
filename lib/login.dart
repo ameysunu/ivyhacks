c7c7e8e4517bb5149947f9aac64ee9424970c242
Amey Sunu <ameysunu00719@gmail.com>
2020-10-02 00:12:21 +0300

    ui fixes

---
+++ lib/login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "COVID Care",
+          style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
